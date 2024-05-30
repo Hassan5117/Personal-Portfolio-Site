81013bf1cce1c866861c6235e7f1c74710816383
Hassan <hassansyed123@hotmail.com>
2024-05-30 19:30:52 -0400

    Created main card functionality from the about-me page. Will likely need to abstract it into another generic component and then pass data into it to dynamically populate it with section information

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, RouterOutlet, RouterLink],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
 export class AboutMeComponent {
+  // constructor(private router: Router) {}
 
+  // goToCourses() {
+  //   console.log('Navigating to Courses + Skills Learned');
+
+  //   this.router.navigate(['courses'])
+  // }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutMeComponent],
+  imports: [RouterOutlet, AboutMeComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
