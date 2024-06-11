d7bf98261c3317e6151042b61951bd8d63e1f946
Hassan <hassansyed123@hotmail.com>
2024-06-10 21:34:21 -0400

    Added more tiles to the main page. Created pages component. Created CourseItem model

---
+++ src/app/components/page-item/page-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-item.component.html',
+  styleUrl: './page-item.component.css'
+})
+export class PageItemComponent {
+
+}
